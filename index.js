cafe5c81992d29c9ff7c68dd3670b4d2c6c03507
rodionovv <wasyarod@mail.ru>
2019-11-26 09:28:39 +0300

    fix file upload

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const fs = require('fs');
 const express = require('express');
 const uploadfiles = require('express-fileupload');
 
+var file_dir = __dirname + '/files'
+
 const app = express();
 app.listen(9000, () => {
-    console.log('Server is running on port 8000');
+    if (!fs.existsSync(file_dir)) {
+        fs.mkdirSync(file_dir);
+    }
+    console.log('Server is running on port 9000');
 });
 
 app.use(uploadfiles());
-app.use(express.static('templates/graphpage'));
-app.use(express.static('templates/mainpage'));
+app.use('graph/', express.static('templates/graphpage/'));
+app.use(express.static('templates/mainpage/'));
 
 app.get('/', function (req, res) {
     console.log('get');
@@ -20,12 +25,18 @@ app.get('/', function (req, res) {
 
 app.post('/', function(req, res) {
     if (req.files) {
+        console.log(req.files);
         var file = req.files.file,
             filename = file.name;
-        file.mv(__dirname + '/files/' + filename, function (err) {
-            if (err) {
-                console.log(err);
+        if (file.mimetype == 'text/plain') {
+            var file_path = file_dir + '/' + filename;
+            if (!fs.existsSync(file_path)) {
+                file.mv(file_dir + '/' + filename, function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                })
             }
-        })
+        }
     }
 });
